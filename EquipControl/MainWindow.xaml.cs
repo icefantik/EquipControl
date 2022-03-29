21eb917af55e5bf7d59a1c06bf74763bb82d1774
icefantik <43622554+icefantik@users.noreply.github.com>
2022-03-29 09:39:40 +0300

    Update MainWindow.xaml.cs

--- EquipControl/MainWindow.xaml.cs
+++ EquipControl/MainWindow.xaml.cs
@@ -29,30 +29,30 @@ namespace EquipControl
         }
         private void EnterAccount(object sender, RoutedEventArgs e)
         {
-            string textLogin = txtBoxLogin.Text;
-            string textPwd = textBoxPwd.Password;
-            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter();
-            DataTable table = new DataTable();
+            string textLogin = txtBoxLogin.Text; //Текст из поля для ввода логина
+            string textPwd = textBoxPwd.Password; //Текст из поля для ввода пароля
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(); // Переменная для подключения к базе данных и исполнения запроса
+            DataTable table = new DataTable(); // Переменная для хранения таблицы из базы данных
 
-            string query = $"SELECT {Database.ColUserLogin}, {Database.ColUserPwd} FROM {Database.TablUsers} WHERE {Database.ColUserLogin} = \'{textLogin}\'";
-            SqlCommand command = new SqlCommand(query, database.getConnection());
-            sqlDataAdapter.SelectCommand = command;
-            sqlDataAdapter.Fill(table);
+            string query = $"SELECT {Database.ColUserLogin}, {Database.ColUserPwd} FROM {Database.TablUsers} WHERE {Database.ColUserLogin} = \'{textLogin}\'"; //Запрос на нахождение логина
+            SqlCommand command = new SqlCommand(query, database.getConnection()); 
+            sqlDataAdapter.SelectCommand = command; // Исполнение запроса к базе данных
+            sqlDataAdapter.Fill(table); //Заполнение переменной данными из таблицы
 
-            if (table.Rows.Count == 1)
+            if (table.Rows.Count == 1) // Проверка получил ли пользователь один логин и пароль
             {
-                if (textLogin == table.Rows[0][0].ToString() && textPwd == table.Rows[0][1].ToString()) {
+                if (textLogin == table.Rows[0][0].ToString() && textPwd == table.Rows[0][1].ToString()) { //Проверка на то что введённый логин и пароль совпадают с данными из базы данных
                     this.Hide(); //Закрытие MainWindow
-                    SearchAudience searchAudience = new SearchAudience();
+                    SearchAudience searchAudience = new SearchAudience(); // Создание объекта для перехода на новое окно
                     searchAudience.Owner = this;
                     searchAudience.Show(); //Переход на новое окно
                 } else {
-                    Message.ShowMessage("Введён неправильный логин или пароль", "Warning");
+                    Message.ShowMessage("Введён неправильный логин или пароль", "Warning"); // Сообщение о том что введён не правельный логин и пароль
                 }
             }
             else
             {
-                Message.ShowMessage("Введён неправильный логин или пароль", "Warning");
+                Message.ShowMessage("Введён неправильный логин или пароль", "Warning"); // Сообщение о том что введён не правельный логин и пароль
             }
         }
     }
