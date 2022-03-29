5fabda6436348ea7751ef0d20e17b7a2b4555cd5
icefantik <43622554+icefantik@users.noreply.github.com>
2022-03-29 12:04:48 +0300

    Update SearchAudience.xaml.cs

--- EquipControl/SearchAudience.xaml.cs
+++ EquipControl/SearchAudience.xaml.cs
@@ -19,26 +19,26 @@ namespace EquipControl
     /// </summary>
     public partial class SearchAudience : Window
     {
-        private static string audienceNumber = null;
+        private static string audienceNumber = null; // Номер аудитории
         public SearchAudience()
         {
             InitializeComponent();
-            updateEquipsTable();
+            updateEquipsTable(); // Заполнение таблици всем списком оборудования
         }
 
-        private void Search(object sender, RoutedEventArgs e)
+        private void Search(object sender, RoutedEventArgs e) // Функция срабатывает при нажатии на кнопку Поиск
         {
-            audienceNumber = txtBoxAudienceNum.Text;
-            if (audienceNumber.Length > 0)
+            audienceNumber = txtBoxAudienceNum.Text; // Текст номера аудитории
+            if (audienceNumber.Length > 0) // Проверка, что поле для ввода не пустое
             {
-                updateEquipsTable();
+                updateEquipsTable(); // Заполнение таблици по номеру аудитории
             }
             else
             {
                 Message.ShowMessage("Введите номер аудитории", "Warning");
             }
         }
-        private void SaveTable(object sender, RoutedEventArgs e)
+        private void SaveTable(object sender, RoutedEventArgs e) // Функция сохраняющая изменения
         {
             var len = equipGrid.Items.Count - 1;
             foreach (var item in equipGrid.Items.OfType<Equip>())
@@ -48,29 +48,29 @@ namespace EquipControl
                     $"{Database.ColNameEquip} = \'{item.name}\', " +
                     $"{Database.ColDayOf} = \'{item.DayOf}\', " +
                     $"{Database.ColAudienceNum} = {item.AudienceNum} " +
-                    $"WHERE {Database.ColEquipId} = {item.id}");
+                    $"WHERE {Database.ColEquipId} = {item.id}"); // Обновление всех элементов таблицы
             }
         }
-        private void DelElemFromTable(object sender, RoutedEventArgs e)
+        private void DelElemFromTable(object sender, RoutedEventArgs e) // Функция для удаления элемента
         {
-            string numEquip = txtBoxNumEquip.Text;
-            string query = $"DELETE FROM {Database.TablEquip} WHERE {Database.ColEquipId} = {numEquip}";
-            Database.runQuerty(query);
+            string numEquip = txtBoxNumEquip.Text; // Номер оборудования
+            string query = $"DELETE FROM {Database.TablEquip} WHERE {Database.ColEquipId} = {numEquip}"; // Запрос на поиск оборудования и удаления его
+            Database.runQuerty(query); // Исполнение запроса
             updateEquipsTable(); //Обновление всей таблицы
         }
         private void OpenWindowAddElem(object sender, RoutedEventArgs e)
         {
-            AddElemWindow addElemWindow = new AddElemWindow();
+            AddElemWindow addElemWindow = new AddElemWindow(); // Создание нового обекта окна
             addElemWindow.Owner = this;
-            addElemWindow.Show();
+            addElemWindow.Show(); // Открытие окна
         }
         private void updateEquipsTable() {
             string query;
             if (audienceNumber != null) {
-                query = $"SELECT * FROM {Database.TablEquip} WHERE AudienceNum = {audienceNumber}";
+                query = $"SELECT * FROM {Database.TablEquip} WHERE AudienceNum = {audienceNumber}"; // Вывод оборудования по номеру аудитории
             } 
             else {
-                query = $"SELECT * FROM {Database.TablEquip}";
+                query = $"SELECT * FROM {Database.TablEquip}"; // Вывод всего оборудования
             }
             List<Equip> equips = Database.queryFillingEquip(query);
             equipGrid.ItemsSource = equips;
